b16dcbacd21ea12926dd00970807eeac6e5b3930
Rian <rianlotter@icloud.com>
2023-04-05 00:17:00 +0200

    Small Changes

--- shared/config.lua
+++ shared/config.lua
@@ -8,7 +8,7 @@ Config.AllowAircraft = false -- Set to true if you want it to be enabled in Airc
 Config.ChangeRate = 0.1 -- Set how fast it changes direction
 
 -- Controls (Get the Key numbers from https://docs.fivem.net/docs/game-references/controls)
-Config.UpKey = 127
-Config.DownKey = 126
-Config.LeftKey = 124
-Config.RightKey = 125
+Config.UpKey = 127  -- Numpad 8
+Config.DownKey = 126 -- Numpad 2
+Config.LeftKey = 124  -- Numpad 4
+Config.RightKey = 125  -- Numpad 6
